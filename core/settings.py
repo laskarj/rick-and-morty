90be100b9f41a9edd9cc7a95a03561a4eb65fa6f
laskarj <koks.adekvat@gmail.com>
2024-01-20 20:55:42 +0100

    Connected to PostgreSQL and run migrations.

--- core/settings.py
+++ core/settings.py
@@ -77,13 +77,17 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "rick_and_morty",
+        "USER": "rick_and_morty_user",
+        "PASSWORD": "rick_and_morty_password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
