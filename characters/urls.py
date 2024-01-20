fad264a71238f757c10f7f647811d46d26909fab
laskarj <koks.adekvat@gmail.com>
2024-01-20 12:33:01 +0100

    Added character list endpoint with name field

--- characters/urls.py
+++ characters/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from characters.views import get_random_character_view
+from characters.views import get_random_character_view, CharacterListView
 
 
 urlpatterns = [
-    path("characters/random", get_random_character_view, name="characters_random"),
+    path("characters/random", get_random_character_view, name="characters-random"),
+    path("characters/", CharacterListView.as_view(), name="characters-list"),
 ]
 
 
--- characters/views.py
+++ characters/views.py
@@ -1,7 +1,9 @@
 from random import choice
 
+from django.db.models.query import QuerySet
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -16,3 +18,14 @@ def get_random_character_view(requests: Request) -> Response:
     random_character = Character.objects.get(pk=random_id)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Character.objects.all()
+        name = self.request.query_params.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
